155cb6d531ad17871a7a5b1455fa3180c7e8157c
tng-92 <t.n.george92@gmail.com>
2025-02-07 12:50:06 +0000

    Course Migration

---
+++ database/migrations/2025_02_09_124152_create_course_descriptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_descriptions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id')->index();
+            $table->text('feild_1');
+            $table->taxt('feild_2');
+            $table->taxt('feild_3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_descriptions');
+    }
+};
